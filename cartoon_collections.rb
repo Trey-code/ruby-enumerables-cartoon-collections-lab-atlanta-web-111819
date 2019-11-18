78e3abdc957c77d656c8dcc625d10455c4c1cba4
Treven Womack <keytonwomack@yahoo.com>
2019-11-18 12:31:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,11 +25,23 @@ end
 
 summon_captain_planet(planateer_calls)
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  i = 0 
+  if array.any? {|i| i.length > 4}
+    return true 
+  else 
+   return false 
+   i = i + 1 
+ end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+long_planeteer_calls(call_screams)
+
+cheese_types = ["cheddar","gouda", "camembert"]
+snacks = ["crackers", "gouda", "thyme"]
+
+
+def find_the_cheese(# code an argument here
+  # the array below is here to help)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
